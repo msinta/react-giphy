098c6184a4528acbf8d41c36c15bbc030843007a
mohamed shegow <shegowmo@gmail.com>
2022-12-07 21:12:30 +0900

    created images

--- workshop/src/index.jsx
+++ workshop/src/index.jsx
@@ -1,1 +1,25 @@
 import '../assets/stylesheets/application.scss';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App';
+
+// const Hello = (props) => {
+
+// const {greeting, name} = props;
+//   return <h1>{greeting} {name}!</h1>;
+// };
+
+// class Hello extends React.Component{
+// render() {
+// const {greeting, name} = this.props;
+// return(
+//   <div>
+//     <h1>{greeting}{name}!</h1>
+//   </div>
+// );
+// }
+
+// }
+const container = document.querySelector("#root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
